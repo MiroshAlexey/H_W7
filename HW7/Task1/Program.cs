786aa69b1b0c88506317bc66cb66c0a95be05c15
MiroshAlexey <166262688+MiroshAlexey@users.noreply.github.com>
2024-04-08 14:52:08 +0300

    Add files via upload

---
+++ HW7/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+
+
+int m = -10;
+int n = 10;
+
+CheckAndLaunchRangeNumbers(m, n);
+void CheckAndLaunchRangeNumbers(int m, int n)
+{
+    if (n < m)
+    {
+        Console.Write("Переменная n должна быть больше или равна m!");
+    }
+    else
+    {
+        RangeNumbers(m, n);
+    }
+}
+
+void RangeNumbers(int m, int n)
+{
+    if (n >= m)
+    {
+        if (m - 1 == n)
+        {
+            return;
+        }
+        Console.Write(m);
+        if (m < n)
+        {
+            Console.Write(", ");
+        }
+        RangeNumbers(m + 1, n);
+    }
+}
---
+++ HW7/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 2: Напишите программу вычисления функции
+//  Аккермана с помощью рекурсии. Даны два
+//  неотрицательных числа m и n.
+
+
+int m = 2;
+int n = 6;
+if (m >= 0 && n >= 0)
+{
+    Console.WriteLine(FunctionAckerman(m, n));
+}
+else
+{
+    Console.WriteLine("Значения m и n должны быть больше либо равны 0");
+}
+
+int FunctionAckerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+
+    if (n == 0 && m > 0)
+    {
+        return FunctionAckerman(m - 1, 1);
+    }
+    else
+    {
+        return FunctionAckerman(m - 1, FunctionAckerman(m, n - 1));
+    }
+}
